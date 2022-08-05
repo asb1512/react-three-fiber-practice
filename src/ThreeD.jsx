56f7fd6aa2a008be971dec90d8c0a9134a5b77e6
asb1512 <andrew@olympicmedia.com>
2022-08-05 14:50:31 -0400

    moved 3D logic to its own component

---
+++ src/ThreeD.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useSpring } from '@react-spring/core';
+import { a } from '@react-spring/three';
+import './App.css';
+
+export default function ThreeD() {
+  const [active, setActive] = useState(0);
+
+  const { spring } = useSpring({
+    spring: active,
+    config: {
+      mass: 2,
+      tension: 400,
+      friction: 50,
+      precision: 0.0001,
+    },
+  });
+
+  const scale = spring.to([0, 1], [1, 5]);
+  const rotation = spring.to([0, 1], [0, Math.PI]);
+  const color = spring.to([0, 1], ['#624EA', '#E45858']);
+
+  return (
+    <div id="canvas-container">
+      <Canvas
+        shadows
+        colorManagement
+        camera={{ position: [-10, 10, 10], fov: 35 }}
+      >
+        <ambientLight />
+        <pointLight position={[-10, 10, -10]} castShadow />
+        <a.group position-y={scale}>
+          <a.mesh
+            rotation-y={rotation}
+            scale-x={scale}
+            scale-z={scale}
+            onClick={() => setActive(Number(!active))}
+          >
+            <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+            <a.meshStandardMaterial
+              attach="material"
+              roughness={0.5}
+              color={color}
+            />
+          </a.mesh>
+        </a.group>
+      </Canvas>
+    </div>
+  );
+}
